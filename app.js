253f92b28e39e4de7b259a06b31b7b0ffe3dce07
Mich Ochieng <michochieng@gmail.com>
2018-09-10 17:49:25 +0000

    changes

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ var express     = require("express"),
     flash       = require("connect-flash"),
     passport    = require("passport"),
     LocalStrategy = require("passport-local"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
+    googlepassport = require("passport-google-oauth");
     
+
     
 // route files
 var homepageRoutes = require("./routes/index"),
@@ -21,6 +23,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use("/", homepageRoutes);
 app.use("/login", loginRoutes);
