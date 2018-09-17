2527b828836a3a2802a2a6ddc3e0278ece655a62
Mich Ochieng <michochieng@gmail.com>
2018-09-17 06:41:48 +0000

    add hover effects and link button to images

--- app.js
+++ app.js
@@ -23,13 +23,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
+// app.use(morgan('dev')); // log every request to the console
+// app.use(cookieParser()); // read cookies (needed for auth)
 
-app.use(session({ secret: 'momo' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+// app.use(session({ secret: 'momo' })); // session secret
+// app.use(passport.initialize());
+// app.use(passport.session()); // persistent login sessions
+// app.use(flash()); // use connect-flash for flash messages stored in session
 
 app.use("/", homepageRoutes);
 app.use("/login", loginRoutes);
